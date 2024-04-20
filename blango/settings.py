98adb09acfec026c4ac70bca46ed50c494b19c19
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2024-04-20 05:25:36 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'blog',
+
+    'crispy_forms','crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
